9dd120f4531cca8613de1c4eb69cd68bc6a68b26
Knitschi <knitschi@freenet.de>
2018-11-25 16:05:44 +0100

    Adjust to changes in CPFCMake api.

---
+++ Sources/cpfPackages.cmake
@@ -0,0 +1,10 @@
+set( CPF_PACKAGES 
+    EXTERNAL cotire
+    EXTERNAL CPFCMake
+    EXTERNAL CPFBuildscripts
+    OWNED DPackage
+    OWNED CPackage
+    OWNED BPackage
+    OWNED APackage
+    OWNED documentation
+)

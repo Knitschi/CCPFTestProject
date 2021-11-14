888ef4f5c3a638c38f3dd4994bfd6aa05ac247ce
Knitschi <knitschi.public@freenet.de>
2021-11-14 22:09:22 +0100

    - Adjusts to changes in CPCMake interface. - Moves former packages as package-components in new package.

--- Sources/packages.cmake
+++ Sources/packages.cmake
@@ -2,10 +2,5 @@ set( CPF_PACKAGES
     EXTERNAL cotire
     EXTERNAL CPFCMake
     EXTERNAL CPFBuildscripts
-    OWNED EPackage
-    OWNED DPackage
-    OWNED CPackage
-    OWNED BPackage
-    OWNED APackage
-    OWNED documentation
+    OWNED CCPFTestPackage
 )

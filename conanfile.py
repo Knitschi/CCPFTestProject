1261fb8acc7ed0d23584a47e10b5e7280b84f180
Knitschi <knitschi.public@freenet.de>
2022-02-03 19:06:03 +0100

    Updates packages and adds a conanfile.

---
+++ conanfile.py
@@ -0,0 +1,27 @@
+from conans import ConanFile
+
+class CCPFTestProject(ConanFile):
+    
+    url = "https://github.com/Knitschi/CCPFTestProject"
+    license = "MIT"
+    description = "A package that is created by the CCPFTestProject repository."
+
+    # Dependencies
+    python_requires = "CPFConanfile/0.0.16@knitschi/development",
+    python_requires_extend = "CPFConanfile.CPFBaseConanfile",
+
+    cpf_conanfile_module = None
+
+    def init(self):
+
+        self.cpf_conanfile_module = self.python_requires["CPFConanfile"].module
+
+        self.cpf_conanfile_module.init_impl(
+            self,
+            self.cpf_conanfile_module.CPFBaseConanfile,
+            "https://github.com/Knitschi/CCPFTestProject.git",
+            path_CPFCMake='Sources/CPFCMake',
+            path_CPFBuildscripts='Sources/CPFBuildscripts',
+            path_CIBuildConfigurations='Sources/CIBuildConfigurations'
+            )
+
